6f84e225b2d84bac0cbf8dfa8ec24507fde8fb19
Avesek <avesekxthaa@gmail.com>
2026-03-11 09:06:56 +0545

    Handle conflict

--- server/config/constants/index.py
+++ server/config/constants/index.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 database_url = os.environ["DATABASE_URL"]
 jwt_private_key = os.environ["JWT_PRIVATE_KEY"]
-mail_password = os.environ["MAIL_PASSWORD"]
 mail_id = os.environ["MAIL_ID"]
+mail_password = os.environ["MAIL_PASSWORD"]
+
+
+
 
