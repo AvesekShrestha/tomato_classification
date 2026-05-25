b8f5f0e638b461b1fbf897fc940c3538f1896fab
Avesek <avesekxthaa@gmail.com>
2026-05-25 08:23:04 +0545

    fix : model architecutre

--- server/utils/models/index.py
+++ server/utils/models/index.py
@@ -1,111 +1,43 @@
-import torch.nn.functional as F 
-import torch
-import math
 import torch.nn as nn
-from torch.nn.grad import conv2d_input, conv2d_weight
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
 
-class LinearFunction(torch.autograd.Function) :
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
 
-  @staticmethod
-  def forward(ctx, input, weight, bias=None) :
-    ctx.save_for_backward(input, weight, bias)
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
 
-    output = F.linear(input, weight, bias)
-    return output
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
 
-  @staticmethod
-  def backward(ctx, grad_output):  # type: ignore[override]
-    input, weight, bias = ctx.saved_tensors
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
 
-    grad_input = grad_output @ weight
-    grad_weight = grad_output.T @ input
-    grad_bias = torch.sum(grad_output, dim=0)
+            nn.Conv2d(256, 512, 3),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
 
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512*6*6, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, num_classes)
+        )
 
-    return grad_input, grad_weight, grad_bias
-
-class ConvolutionFunction(torch.autograd.Function) :
-
-  @staticmethod
-  def forward(ctx, input, weight, bias=None, stride = 1, padding = 0) :
-    ctx.save_for_backward(input, weight, bias)
-    ctx.stride = stride
-    ctx.padding = padding
-
-    output = F.conv2d(input, weight, bias, stride, padding)
-    return output
-
-  @staticmethod
-  def backward(ctx, grad_output) :  # type: ignore[override]
-    input, weight, bias = ctx.saved_tensors
-    stride = ctx.stride
-    padding = ctx.padding
-
-    grad_input = conv2d_input(input.shape, weight, grad_output, stride, padding)
-    grad_weight = conv2d_weight(input, weight.shape, grad_output, stride, padding)
-    grad_bias = torch.sum(grad_output, dim=(0,2,3))
-
-    return grad_input, grad_weight, grad_bias, None, None
-
-class Dense(nn.Module):
-
-  def __init__(self, input_size, output_size):
-    super(Dense, self).__init__()
-    self.weights = nn.Parameter(torch.empty(output_size, input_size, device=device))
-    self.bias = nn.Parameter(torch.zeros(output_size, device=device))
-    nn.init.kaiming_uniform_(self.weights, a=math.sqrt(5))
-
-  def forward(self, input) :
-    return LinearFunction.apply(input, self.weights, self.bias)
-
-class Convolution(nn.Module) :
-
-  def __init__(self, in_channel, out_channel, kernel_size) :
-    super(Convolution, self).__init__()
-    self.kernels = nn.Parameter(torch.empty(out_channel, in_channel, kernel_size, kernel_size, device=device))
-    self.bias = nn.Parameter(torch.zeros(out_channel, device=device))
-    nn.init.kaiming_uniform_(self.kernels, a=math.sqrt(5))
-
-  def forward(self, input) :
-    return ConvolutionFunction.apply(input, self.kernels, self.bias)
-
-class MaxPool(nn.Module) :
-  def __init__(self, kernel_size) :
-    super(MaxPool, self).__init__()
-    self.kernel_size = kernel_size
-
-  def forward(self, input) :
-    output = F.max_pool2d(input, self.kernel_size)
-    return output
-
-class NeuralNet(nn.Module) :
-
-  def __init__(self):
-    super(NeuralNet, self).__init__()
-
-    self.conv1 = Convolution(3, 32, 3)
-    self.conv2 = Convolution(32, 64, 3)
-    self.conv3 = Convolution(64, 128, 3)
-    self.conv4 = Convolution(128, 256, 3)
-    self.conv5 = Convolution(256, 512, 3)
-    self.pool = MaxPool(2)
-
-    self.fc1 = Dense(6*6*512,1024)
-    self.fc2 = Dense(1024, 64)
-    self.fc3 = Dense(64, 10)
-    self.flatten = nn.Flatten(start_dim=1)
-
-  def forward(self, x) :
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
-    x = self.pool(F.relu(self.conv3(x)))
-    x = self.pool(F.relu(self.conv4(x)))
-    x = self.pool(F.relu(self.conv5(x)))
-
-    x = self.flatten(x)
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = self.fc3(x)
-    return x
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
--- server/utils/models/model_loader.py
+++ server/utils/models/model_loader.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import torch
 from utils.errors.index import InternalServerError
-from utils.models.index import NeuralNet
+from utils.models.index import CNN
 
 
 file_path = Path("/home/avesek/Documents/workspace/collage/seventh/project/tomato_classification/model/tomato_state_dict.pth").resolve()
 model = None
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_model() : 
     global model
@@ -14,11 +15,10 @@ def load_model() :
     if not file_path.exists() : 
         raise InternalServerError("No model found")
 
-    if model is None : 
-        model = NeuralNet()
-        model.load_state_dict(torch.load(file_path, map_location=torch.device("cpu")))
-        model.eval()
-    return model
+    if model is None :
+        model = CNN(num_classes=10)
+        model.load_state_dict(torch.load(file_path, map_location=device, weights_only=True))
+        return model
 
 def get_model() : 
 
